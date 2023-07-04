250b5bdabd4495be972596bd58a86fe7c2da9e69
gdariodh <diazhernandezgabriel2001@gmail.com>
2023-07-04 00:57:29 -0400

    added stage manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,62 @@
 
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+   public Player player;
+   public Text scoreText;
+   public GameObject playButton;
+   public GameObject gameOver;
+
    private int score;
+
+   private void Awake()
+   {
+
+     Application.targetFrameRate = 60;
+
+     Pause();
+   }
+
+   public void Play()
+   {
+    score = 0;
+    scoreText.text = $"Score: {score.ToString()}";
+
+    playButton.setActive(false);
+    gameOver.SetActive(false);
+    
+    Time.timeScale = 1f;
+    player.enabled = true;
+
+    Pipes[] pipes = FindObjectsOfType<Pipes>();
+
+    for (int i = 0; i < pipes.Length; i++)
+    {
+       Destroy(pipes[i].gameObject);
+    }
+   }
+
+   public void Pause()
+   {
+     Time.timeScale = 0f;
+     player.enabled = false;
+   }
 
    public void GameOver()
    {
-     Debug.Log("Game Over!");   
+     Debug.Log("Game Over!");  
+     gameOver.SetActive(true);
+     playButton.SetActive(true);
+
+     Pause(); 
    }
 
    public void IncreaseScore()
    {    
      score++;
+     scoreText.text = $"Score: {score.ToString()}";
    }
 
  
