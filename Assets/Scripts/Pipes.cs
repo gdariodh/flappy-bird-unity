8f690c3b67314e07837c913425c1ae5527f003d2
gdariodh <diazhernandezgabriel2001@gmail.com>
2023-07-03 23:52:51 -0400

    added pipes logic

--- Assets/Scripts/Pipes.cs
+++ Assets/Scripts/Pipes.cs
@@ -1,18 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Pipes : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed = 5f;
+    private float leftEdge;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        leftEdge = Camera.main.ScreenToWorldPoint(Vector3.zero).x - 1f;
+    }
+    
+    private void Update()
+    {
+        transform.position += Vector3.left * speed * Time.deltaTime;
+
+        if(transform.position.x < leftEdge)
+        {
+            Destroy(gameObject);
+        }
     }
 }
